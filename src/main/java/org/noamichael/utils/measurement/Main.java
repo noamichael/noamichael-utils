b34a2cc4f2ea6c99852ae13624eeecf14b2d9d87
michael <michael@michael-desktop>
2014-09-10 20:58:36 -0400

    Wrote methods to fetch begin/end tag positions

--- src/main/java/org/noamichael/utils/measurement/Main.java
+++ src/main/java/org/noamichael/utils/measurement/Main.java
@@ -1,5 +1,8 @@
 package org.noamichael.utils.measurement;
 
+import org.noamichael.utils.xml.DefaultXmlParserImpl;
+import org.noamichael.utils.xml.api.XmlParser;
+
 /**
  *
  * @author michael
@@ -13,5 +16,26 @@ public class Main {
         dimensions.addValue(Dimension.Imperial.INCHES, 100);
         Dimension dimension2 = new Dimension();
         dimension2.addValue(Dimension.Imperial.MILES, 3);
+        XmlParser xmlParser = new DefaultXmlParserImpl();
+        xmlParser.getParsedXmlDocument("<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
+"    <modelVersion>4.0.0</modelVersion>\n" +
+"    <groupId>org.noamichael</groupId>\n" +
+"    <artifactId>noamichael-utils</artifactId>\n" +
+"    <version>1.0-SNAPSHOT</version>\n" +
+"    <packaging>jar</packaging>\n" +
+"    <dependencies>\n" +
+"        <dependency>\n" +
+"            <groupId>javax</groupId>\n" +
+"            <artifactId>javaee-web-api</artifactId>\n" +
+"            <version>7.0</version>\n" +
+"            <scope>provided</scope>\n" +
+"        </dependency>\n" +
+"    </dependencies>\n" +
+"    <properties>\n" +
+"        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>\n" +
+"        <maven.compiler.source>1.8</maven.compiler.source>\n" +
+"        <maven.compiler.target>1.8</maven.compiler.target>\n" +
+"    </properties>\n" +
+"</project>");
     }
 }
--- src/main/java/org/noamichael/utils/xml/DefaultXmlParserImpl.java
+++ src/main/java/org/noamichael/utils/xml/DefaultXmlParserImpl.java
@@ -1,5 +1,7 @@
 package org.noamichael.utils.xml;
 
+import java.util.ArrayList;
+import java.util.List;
 import static org.noamichael.utils.se.StringUtil.isNullOrEmpty;
 import org.noamichael.utils.xml.api.ParseException;
 import org.noamichael.utils.xml.api.ParsedXmlDocument;
@@ -8,17 +10,18 @@ import org.noamichael.utils.xml.api.XmlParser;
 
 public class DefaultXmlParserImpl implements XmlParser {
 
+    private static final String XML_DECLARATION = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+
     private DefaultParsedXmlDocument document;
-    private boolean foundBeginTag;
-    private boolean foundEndTag;
     private XmlElement currentParentElement;
 
     protected enum CharType {
 
-        BEGIN_TAG, END_TAG, EQUALS, SINGLE_QUOTE, DOUBLE_QUOTE, CDATA;
+        BEGIN_TAG, END_TAG, EQUALS, SINGLE_QUOTE, DOUBLE_QUOTE, CDATA, SPACE, SLASH;
     }
 
     protected enum ParsePhase {
+
         READING_TAG;
     }
 
@@ -28,10 +31,108 @@ public class DefaultXmlParserImpl implements XmlParser {
             throw new ParseException("Cannot parse null or empty xml.");
         }
         document = new DefaultParsedXmlDocument();
-        
+        int[] positions = getStartTagPositions(rawXml.toCharArray());
+        String beginTag = rawXml.substring(positions[0], positions[1] + 1);
+        System.out.println(beginTag);
+        System.out.println(getTagName(beginTag.toCharArray()));
+
+        int[] endTagPosition = getEndTagPositions(rawXml.toCharArray());
+        String endTag = rawXml.substring(endTagPosition[0], endTagPosition[1] + 1);
+        System.out.println(endTag);
+        System.out.println(getTagName(endTag.toCharArray()));
+
         return document;
     }
-    
+
+    private void collectTags(char[] rawData) {
+        List<SemiParsedElement> semiParsedElements = new ArrayList();
+        int i = 0;
+
+    }
+
+    private int[] getStartTagPositions(char[] rawData) {
+        int currentIndex = 0;
+        int beginTagPosition = -1;
+        int endTagPosition = -1;
+        boolean kill = false;
+        boolean foundBeginningTag = false;
+        while (currentIndex < rawData.length && !kill) {
+            CharType charType = getCharType(rawData[currentIndex]);
+            switch (charType) {
+                case BEGIN_TAG: {
+                    if (foundBeginningTag == true) {
+                        throw new ParseException("The beginning of the tag was already found.");
+                    }
+                    beginTagPosition = currentIndex;
+                    foundBeginningTag = true;
+                    break;
+                }
+                case END_TAG: {
+                    if (foundBeginningTag == false) {
+                        throw new ParseException("Found < before >");
+                    }
+                    endTagPosition = currentIndex;
+                    kill = true;
+                    continue;
+                }
+            }
+            currentIndex++;
+        }
+        return new int[]{beginTagPosition, endTagPosition};
+    }
+
+    private int[] getEndTagPositions(char[] rawData) {
+        int currentIndex = rawData.length - 1;
+        int beginTagPosition = -1;
+        int endTagPosition = -1;
+        boolean kill = false;
+        boolean foundEndTag = false;
+        while (currentIndex >= 0 && !kill) {
+            CharType charType = getCharType(rawData[currentIndex]);
+            switch (charType) {
+                case BEGIN_TAG: {
+                    if (!foundEndTag) {
+                        throw new ParseException("Found the first tag before the last!");
+                    }
+                    beginTagPosition = currentIndex;
+                    if (!peakForward(rawData, currentIndex).equals(CharType.SLASH)) {
+                        System.out.println("Non-single-endtag");
+                    }
+                    kill = true;
+                    continue;
+                }
+                case END_TAG: {
+                    if (foundEndTag) {
+                        throw new ParseException("The end of the tag was already found");
+                    }
+                    foundEndTag = true;
+                    endTagPosition = currentIndex;
+                    break;
+                }
+            }
+            currentIndex--;
+        }
+        return new int[]{beginTagPosition, endTagPosition};
+    }
+
+    private CharType peakForward(char[] data, int index) {
+        if (index + 1 >= data.length) {
+            throw new ParseException("Tried to peak forward out of index.");
+        }
+        return getCharType(data[index + 1]);
+    }
+
+    protected String getTagName(char[] rawData) {
+        String tagName = "";
+        int i = 1;
+        while (i < rawData.length && rawData[i] != ' ') {
+            if (rawData[i] != '/' && rawData[i] != '>') {
+                tagName += rawData[i];
+            }
+            i++;
+        }
+        return tagName;
+    }
 
     protected CharType getCharType(char c) {
         switch (c) {
@@ -47,13 +148,114 @@ public class DefaultXmlParserImpl implements XmlParser {
             case '>': {
                 return CharType.END_TAG;
             }
-            case '=':{
+            case '=': {
                 return CharType.EQUALS;
+            }
+            case ' ': {
+                return CharType.SPACE;
+            }
+            case '/': {
+                return CharType.SLASH;
             }
             default: {
                 return CharType.CDATA;
             }
         }
+    }
+
+    protected class SemiParsedElement {
+
+        private int startTagBegin;
+        private int startTagEnd;
+        private int endTagBegin;
+        private int endTagEnd;
+        private String tagName;
+        private String innerContent;
+
+        /**
+         * @return the startTagBegin
+         */
+        public int getStartTagBegin() {
+            return startTagBegin;
+        }
+
+        /**
+         * @param startTagBegin the startTagBegin to set
+         */
+        public void setStartTagBegin(int startTagBegin) {
+            this.startTagBegin = startTagBegin;
+        }
+
+        /**
+         * @return the startTagEnd
+         */
+        public int getStartTagEnd() {
+            return startTagEnd;
+        }
+
+        /**
+         * @param startTagEnd the startTagEnd to set
+         */
+        public void setStartTagEnd(int startTagEnd) {
+            this.startTagEnd = startTagEnd;
+        }
+
+        /**
+         * @return the endTagBegin
+         */
+        public int getEndTagBegin() {
+            return endTagBegin;
+        }
+
+        /**
+         * @param endTagBegin the endTagBegin to set
+         */
+        public void setEndTagBegin(int endTagBegin) {
+            this.endTagBegin = endTagBegin;
+        }
+
+        /**
+         * @return the endTagEnd
+         */
+        public int getEndTagEnd() {
+            return endTagEnd;
+        }
+
+        /**
+         * @param endTagEnd the endTagEnd to set
+         */
+        public void setEndTagEnd(int endTagEnd) {
+            this.endTagEnd = endTagEnd;
+        }
+
+        /**
+         * @return the tagName
+         */
+        public String getTagName() {
+            return tagName;
+        }
+
+        /**
+         * @param tagName the tagName to set
+         */
+        public void setTagName(String tagName) {
+            this.tagName = tagName;
+        }
+
+        /**
+         * @return the innerContent
+         */
+        public String getInnerContent() {
+            return innerContent;
+        }
+
+        /**
+         * @param innerContent the innerContent to set
+         */
+        public void setInnerContent(String innerContent) {
+            this.innerContent = innerContent;
+        }
+
     }
 
 }
